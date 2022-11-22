70e8c9a7bae899f61dddd7fd7ffcb0229fed5022
2006239 <2006239@edu.karelia.fi>
2022-11-22 15:31:30 +0200

    new file:   .ELM327_log.txt.swp 	new file:   .ELM327cat.log.swp 	new file:   .elm327.txt.swp 	new file:   .elm327_log.txt.swp 	new file:   ELM327_log.txt 	new file:   ble-serial/ble-venv/bin/gpsdclient 	new file:   elm327.txt 	new file:   elm327async.py 	new file:   gps.py

---
+++ gps.py
@@ -0,0 +1,7 @@
+from gpsdclient import GPSDClient
+
+client = GPSDClient(host="127.0.0.1")
+for result in client.dict_stream(convert_datetime=True, filter=["TPV"]):
+	print("<time> %s" % result.get("time", "")+ "</time>")
+	print("{ lat: %s" % result.get("lat", "")+ ", long: %s }" % result.get("lon","")) 
+
