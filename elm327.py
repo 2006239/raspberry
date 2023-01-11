2dfc8735bf76ad26cd29285356e9ed94a6a5fb19
2006239 <2006239@edu.karelia.fi>
2023-01-11 15:20:12 +0200

    Update elm327.py

--- elm327.py
+++ elm327.py
@@ -144,9 +144,9 @@ def paivita():
 
 def aloita_lopeta():
     if button['text'] == "Aloita":
+        thread1 = threading.Thread(target=aja(), daemon=True).start()
+        thread2 = threading.Thread(target=paivita(), daemon=True).start()
         button.config(text="Lopeta", command=close_window, fg="red")
-        threading.Thread(target=aja(), daemon=True).start()
-        threading.Thread(target=paivita(), daemon=True).start()
     else:
         button.config(text="Aloita", command=close_window, fg="green")
 
